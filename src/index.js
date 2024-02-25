0ea6606cc67b14a578a3ba2cceb9c6e6d04f58c9
Amitshu2003 <201951021@iiitvadodara.ac.in>
2024-02-25 19:29:26 +0530

    add middleware configs in app.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-
-connectDB();
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed!!! Error: ", err);
+  });
 
 /*
 require("dotenv").config();
