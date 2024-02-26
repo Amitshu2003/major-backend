91b060c89571e3e05c6979dec9fdb78eecfcdcdb
Amitshu2003 <201951021@iiitvadodara.ac.in>
2024-02-26 19:07:53 +0530

    add controller for register a user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,66 @@
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
 const registerUser = async (req, res) => {
-  res.status(200).json({ message: "hi" });
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists : username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token fields from response
+  // check for user creation
+  // return res
+
+  const { username, email, fullName, password } = req.body;
+  if (!username || !email || !fullName || !password) {
+    // throw new Error("All fields are required");
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    return res
+      .status(409)
+      .json({ error: "User with email or username already exists" });
+  }
+
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+  if (!avatarLocalPath) {
+    return res.status(400).json({ error: "Avatar file is required" });
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    return res.status(400).json({ error: "Avatar file is required" });
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -watchHistory"
+  );
+  if (!createdUser) {
+    return res
+      .status(500)
+      .json({ error: "Something went wrong while registering the user" });
+  }
+
+  return res.status(201).json({ createdUser });
 };
 
 export { registerUser };
